8b99c6df16bbe99e545db5d33e35056778d091d3
systruss <systruss@gmail.com>
2021-08-27 17:04:40 +0000

    updating register command to accept file input

--- src/Commands/Register.php
+++ src/Commands/Register.php
@@ -83,6 +83,7 @@ class Register extends Command
 			// Output one line until end-of-file
 			while(!feof($walletsfile)) {
 				$wallet = fgets($walletsfile);
+				echo "-----------   $wallet";
 				list($network,$passphrase) = explode('-',$wallet);
 				echo "network = $network \n";
 			}
