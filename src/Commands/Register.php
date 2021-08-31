e53f9217488f371a1fd79de167731c607ff6fe80
systruss <systruss@gmail.com>
2021-08-31 14:15:14 +0000

    update branch multiwallet

--- src/Commands/Register.php
+++ src/Commands/Register.php
@@ -105,53 +105,54 @@ class Register extends Command
 				}
 			}	
 			fclose($walletsfile);
-		}
-		while (1 == 1) {
-			$network = $this->ask('select network [ (1) infi or (2) Hedge]: ');
-			echo "\n";
-			switch ($network) {
-				case "1":
-						echo "you selected infi\n";
-					$network = 'infi';
-					$quit=1;
-						break;
-				case "2":
-						echo "you selected Hedge\n";
-					$network = 'edge';
-					$quit=1;
-						break;
-				case "q":
-						echo "you selected to quit\n";
-					$quit=1;
-						break;
-				default:
-						echo "please select 1 for infi or 2 for Hedge or q to quit \n";
-					$quit = 0;
+		}else {
+			while (1 == 1) {
+				$network = $this->ask('select network [ (1) infi or (2) Hedge]: ');
+				echo "\n";
+				switch ($network) {
+					case "1":
+							echo "you selected infi\n";
+						$network = 'infi';
+						$quit=1;
+							break;
+					case "2":
+							echo "you selected Hedge\n";
+						$network = 'edge';
+						$quit=1;
+							break;
+					case "q":
+							echo "you selected to quit\n";
+						$quit=1;
+							break;
+					default:
+							echo "please select 1 for infi or 2 for Hedge or q to quit \n";
+						$quit = 0;
+					}
+
+				if ($quit == 1) {
+					break;
 				}
-
-			if ($quit == 1) {
-				break;
 			}
-		}
-		
-		//
-		//network is select now provide passphrase
-		//
-		while (1 == 1) {
-			$passphrase = $this->ask('please enter a passphrase : ');
-			$this->info("you provided the following passphrase :  $passphrase ");
-			$confirm = $this->confirm('Is it correct [y/n] or q to quit : ');
-			if ($confirm) { break;}
-		}
+			
+			//
+			//network is select now provide passphrase
+			//
+			while (1 == 1) {
+				$passphrase = $this->ask('please enter a passphrase : ');
+				$this->info("you provided the following passphrase :  $passphrase ");
+				$confirm = $this->confirm('Is it correct [y/n] or q to quit : ');
+				if ($confirm) { break;}
+			}
 
-		//register delegate 
-		$delegate = new Delegate();
-		$success = $delegate->register($passphrase,$network);
-		
-		if (!$success) 
-		{
-			$this->info("error while registering delegate");
-			return false;
-		}
-	}	
+			//register delegate 
+			$delegate = new Delegate();
+			$success = $delegate->register($passphrase,$network);
+			
+			if (!$success) 
+			{
+				$this->info("error while registering delegate");
+				return false;
+			}
+		}	
+	}
 }
